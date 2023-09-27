b1b2da4a825d7b154bd45518db1a0f12c5c11b32
thejayadad <thejayadad@gmail.com>
2023-09-27 09:12:53 -0400

    newpage

--- myapp/app/account/new/page.js
+++ myapp/app/account/new/page.js
@@ -9,9 +9,14 @@ import { useSession } from 'next-auth/react'
 const NewRide = () => {
     const [title, setTitle] = useState('')
     const [desc, setDesc] = useState('')
-    const [category, setCategory] = useState("Nature")
+    const [category, setCategory] = useState("")
     const [photo, setPhoto] = useState('')
     const [perks, setPerks] = useState('')
+    const [extraInfo,setExtraInfo] = useState('');
+    const [checkIn,setCheckIn] = useState('');
+    const [checkOut,setCheckOut] = useState('');
+    const [seats,setSeats] = useState(1);
+    const [price,setPrice] = useState(100);
 
     const { data: session, status } = useSession()
     const router = useRouter()
