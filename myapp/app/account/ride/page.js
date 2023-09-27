4d90e5d060c66bef05039aed4c1e28cce2402ccd
thejayadad <thejayadad@gmail.com>
2023-09-27 08:25:20 -0400

    icons

--- myapp/app/account/ride/page.js
+++ myapp/app/account/ride/page.js
@@ -1,11 +1,20 @@
 'use client'
 
+import Link from 'next/link'
 import React from 'react'
+import { FaCar } from "react-icons/fa";
+
 
 const Ride = () => {
   return (
-    <section>
-        Ride
+    <section className='px-4 flex flex-col'>
+    <div className="flex">
+      <Link href="/">
+        <span className="flex items-center px-4 py-2 text-white bg-purple-400 rounded-sm hover:bg-purple-600 transition duration-300 ease-in-out">
+          <FaCar className="mr-2" /> Add Ride
+        </span>
+      </Link>
+    </div>
     </section>
   )
 }
--- myapp/components/AccountNavbar.js
+++ myapp/components/AccountNavbar.js
@@ -2,6 +2,7 @@
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { FaBook, FaCar } from "react-icons/fa"; // Import the React icons you want to use
 
 
 const AccountNavbar = () => {
@@ -18,22 +19,37 @@ const AccountNavbar = () => {
             Access Denied
         </p>
     }
-    const isActive = (path) => {
-        return router.pathname === path ? "bg-purple-400 px-4 text-indigo-600" : "text-gray-600";
-      };
+    const AccountNavbar = ({ session }) => {
+        const isActive = (path) => {
+          return router.pathname === path ? "bg-purple-400 px-4 text-indigo-600" : "text-gray-600";
+        };
+
   return (
     <section className="flex justify-around items-center px-4">
       <h1 className="cursor-pointer">Hello, {session.user.email}!</h1>
       <nav className="flex">
-      <Link className={`text-sm mr-2 ${isActive("/account/bookings")}`} href="/account/bookings">
-        Bookings</Link>
-      <Link className={`text-sm mr-4 ${isActive("/account/ride")}`} href="/account/ride">
-        Rides</Link>
+        <NavItem path="/account/bookings" currentPath={router.pathname}>
+          <FaBook className="mr-2" /> Bookings
+        </NavItem>
+        <NavItem path="/account/ride" currentPath={router.pathname}>
+          <FaCar className="mr-2" /> Rides
+        </NavItem>
       </nav>
     </section>
   )
 }
-
+const NavItem = ({ path, currentPath, children }) => {
+    const isActive = path === currentPath;
+    const className = isActive
+      ? "bg-purple-400 px-4 text-indigo-600"
+      : "text-gray-600";
+}
+    return (
+      <Link href={path}>
+        <span className={`text-sm mr-2 ${className}`}>{children}</span>
+      </Link>
+    );
+  };
 
 
 export default AccountNavbar
